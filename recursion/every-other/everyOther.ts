4a7ec49e1a6daecb41b3a7d04f501df766c5f9dd
Crystal Tran <c.t.tran95@gmail.com>
2024-02-27 19:52:45 -0500

    solved every other, working on longest

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,11 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  if (str.length === 0) return str;
+
+  // console.log("str[0]", str[0]);
+  return str[0] + everyOther(str.slice(2))
+
 }
 
 export { everyOther };
--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -1,9 +1,14 @@
 import { LLNodeStr } from "../common/ll";
 
+// const words = new LLStr(["hello", "hi", "hola"]);
+// longest(new LLStr(words));  // 5
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  if(words === null) return 0;
+
+  // return Math.max(words.val.length, longest(words.next))
+
 }
 
 export { longest };
