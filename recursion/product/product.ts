0a45eb6ea3c56a21e6d4c845ce9444ac5658cfe7
Victor Kim <vskimjr@gmail.com>
2024-02-27 19:26:34 -0500

    completed product

--- recursion/product/product.ts
+++ recursion/product/product.ts
@@ -3,7 +3,9 @@ import { LLNodeNum } from "../common/ll";
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+  if (nums === null) return 1
+
+  return nums.val * product(nums.next)
 }
 
 export { product };
